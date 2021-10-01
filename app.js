3fb8e04ec843291ef739cef1d03755bdb344e285
unknown <danguts23@gmail.com>
2021-10-02 02:11:29 +0300

    made possible storing tasks to local storage

--- app.js
+++ app.js
@@ -1,33 +1,42 @@
 const itemContainer = document.querySelector('.item-container');
-const items = document.querySelectorAll('.item');
+let items = [];
 const addBtn = document.querySelector('#add-item');
 
 const addNewTask = (task) => {
-  itemContainer.insertAdjacentHTML('beforeend', `<div class="item"><input type="checkbox" /><button>X</button></div>`);
+  itemContainer.insertAdjacentHTML('beforeend', `<div class="item" id=${localStorage.length - 1}><input type="checkbox" /><button>X</button></div>`);
   itemContainer.lastChild.insertAdjacentText('beforeend', task);
+
+  // check listener
   itemContainer.lastChild.firstChild.addEventListener('change', (event) => {
     event.target.parentNode.classList.toggle('checked');
   })
+
+  // delete listener
   itemContainer.lastChild.childNodes[1].addEventListener('click', (event) => {
     itemContainer.removeChild(event.target.parentNode);
+    localStorage.removeItem(event.target.parentNode.id);
+    if (!localStorage.length) {
+      itemContainer.insertAdjacentHTML('afterbegin', '<p>Your To Do List is empty. Make a task by cliking the button below!</p>')
+    }
   })
 }
 
-items.forEach(item => {
-  const checkbox = item.querySelector('input');
-  const deleteBtn = item.querySelector('button');
-
-  checkbox.addEventListener('change', () => {
-    item.classList.toggle('checked');
-  })
-
-  deleteBtn.addEventListener('click', (event) => {
-    itemContainer.removeChild(event.target.parentNode);
-  })
-});
+if (localStorage.length) {
+  for (let i = 0; i < localStorage.length; i++) {
+    addNewTask(localStorage.getItem(i));
+  }
+} else {
+  itemContainer.insertAdjacentHTML('afterbegin', '<p>Make your first To Do!</p>')
+}
 
 addBtn.addEventListener('click', () => {
   const task = prompt('New Task');
+  if (!localStorage.length) {
+    itemContainer.firstChild.remove();
+  }
 
+  localStorage.setItem(localStorage.length, task);
+  console.log(localStorage);
   addNewTask(task);
 })
+
